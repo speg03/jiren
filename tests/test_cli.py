94e38b2da372956a6fe2740a1182f53c4483762b
Takahiro Yano <speg03@gmail.com>
2019-07-20 17:01:43 +0900

    :art: Separate tests of Application class and tests of main function

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,10 +4,10 @@ import tempfile
 
 import pytest
 
-from jiren.cli import main
+from jiren.cli import Application, main
 
 
-class TestCLI:
+class TestApplication:
     @pytest.mark.parametrize(
         "inputs,argv,expected",
         [
@@ -16,7 +16,7 @@ class TestCLI:
             ("{{ greeting | default('hi') }}", [], "hi\n"),
         ],
     )
-    def test_main(self, monkeypatch, inputs, argv, expected):
+    def test_run(self, monkeypatch, inputs, argv, expected):
         argv = ["jiren"] + argv
         stdin = io.StringIO(inputs)
         stdout = io.StringIO()
@@ -25,16 +25,15 @@ class TestCLI:
             m.setattr("sys.argv", argv)
             m.setattr("sys.stdin", stdin)
             m.setattr("sys.stdout", stdout)
-            main()
+            Application().run()
 
-        actual = stdout.getvalue()
-        assert expected == actual
+        assert stdout.getvalue() == expected
 
     @pytest.mark.parametrize(
         "inputs,argv,expected",
         [("{{ greeting }}", ["--var.greeting=hello"], "hello\n")],
     )
-    def test_main_with_infile(self, monkeypatch, inputs, argv, expected):
+    def test_run_with_infile(self, monkeypatch, inputs, argv, expected):
         template_dir = tempfile.TemporaryDirectory(prefix="jiren-")
         template_file = os.path.join(template_dir.name, "template.j2")
         with open(template_file, "w") as f:
@@ -46,7 +45,21 @@ class TestCLI:
         with monkeypatch.context() as m:
             m.setattr("sys.argv", argv)
             m.setattr("sys.stdout", stdout)
+            Application().run()
+
+        assert stdout.getvalue() == expected
+
+
+class TestCLI:
+    def test_main(self, monkeypatch):
+        argv = ["jiren", "--var.greeting=hello"]
+        stdin = io.StringIO("{{ greeting }}")
+        stdout = io.StringIO()
+
+        with monkeypatch.context() as m:
+            m.setattr("sys.argv", argv)
+            m.setattr("sys.stdin", stdin)
+            m.setattr("sys.stdout", stdout)
             main()
 
-        actual = stdout.getvalue()
-        assert expected == actual
+        assert stdout.getvalue() == "hello\n"
