383d3fd9fea076f67205c1ef72c54e78b58e4b6c
Takahiro Yano <speg03@gmail.com>
2021-01-26 19:02:32 +0900

    Coverage 100% for Python 3.8+

--- jiren/__init__.py
+++ jiren/__init__.py
@@ -2,7 +2,7 @@
 
 try:
     from importlib.metadata import version
-except ImportError:
+except ImportError:  # pragma: no cover
     from importlib_metadata import version
 
 __version__ = version("jiren")
