3933eb1f95aff72c599c3778a446505b2df7c69c
Takahiro Yano <speg03@gmail.com>
2026-08-08 21:08:24 +0900

    Consider the PackageNotFoundError exception
    However, in most cases, this exception should not occur.

--- src/jiren/__init__.py
+++ src/jiren/__init__.py
@@ -1,3 +1,8 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version(__package__) if __package__ else "0+unknown"
+__version__ = "0+unknown"
+if __package__:
+    try:
+        __version__ = importlib.metadata.version(__package__)
+    except importlib.metadata.PackageNotFoundError:  # pragma: no cover
+        pass
